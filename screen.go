5eda10835ee074eff1b8c132517aac76c148c1c4
FreyreCorona <einierfreyre60@gmail.com>
2025-12-12 16:46:10 -0300

    improve performance due XOR of the entire byte

--- screen.go
+++ screen.go
@@ -23,21 +23,48 @@ func (s *Screen) Set(x, y int, on bool) {
 		return
 	}
 
-	byteIndex := y*s.rowBytes + (x / 8)
-	bitPos := 7 - (x % 8)
+	i := y*s.rowBytes + (x / 8)
+	pos := 7 - (x % 8)
 
 	if on {
-		s.buffer[byteIndex] |= 1 << bitPos
-	} else {
-		s.buffer[byteIndex] &^= 1 << bitPos
+		s.buffer[i] |= 1 << pos
+		return
 	}
+
+	s.buffer[i] &^= 1 << pos
 }
 
 func (s *Screen) Draw(x, y int, data []byte) {
+	n := len(s.buffer)
+
 	for row := range len(data) {
-		for bit := range 8 {
-			on := (data[row]>>(7-bit))&1 == 1
-			s.Set(x+bit, y+row, on)
+		dy := y + row
+		if dy < 0 || dy >= s.Height {
+			continue
+		}
+
+		sprByte := data[row]
+		i := dy*s.rowBytes + (x / 8)
+
+		shift := x % 8
+
+		if shift == 0 {
+			if i >= 0 && i < n {
+				s.buffer[i] = s.buffer[i] ^ sprByte
+			}
+		} else {
+			if i < 0 || i >= n {
+				continue
+			}
+
+			l := sprByte >> shift
+			r := sprByte << (8 - shift)
+
+			s.buffer[i] = s.buffer[i] ^ l
+
+			if i+1 < n {
+				s.buffer[i+1] = s.buffer[i+1] ^ r
+			}
 		}
 	}
 }
@@ -47,9 +74,9 @@ func (s *Screen) Render(w io.Writer) error {
 
 	for y := range s.Height {
 		for x := range s.Width {
-			byteIndex := y*s.rowBytes + (x / 8)
-			bitPos := 7 - (x % 8)
-			bit := (s.buffer[byteIndex] >> bitPos & 1)
+			i := y*s.rowBytes + (x / 8)
+			pos := 7 - (x % 8)
+			bit := (s.buffer[i] >> pos) & 1
 
 			if bit == 1 {
 				line[x] = '#'
@@ -58,9 +85,6 @@ func (s *Screen) Render(w io.Writer) error {
 			}
 		}
 		if _, err := w.Write(line); err != nil {
-			return err
-		}
-		if _, err := w.Write([]byte("\n")); err != nil {
 			return err
 		}
 	}
