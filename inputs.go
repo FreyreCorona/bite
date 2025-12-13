1e6b03b06770c9b25c95bbf30cd76f8e66dfd995
FreyreCorona <einierfreyre60@gmail.com>
2025-12-12 22:54:05 -0300

    implement a input device with control to press and release keys

--- inputs.go
+++ inputs.go
@@ -1,1 +1,66 @@
 package main
+
+import "math/bits"
+
+type Keyboard struct {
+	keys []uint16
+	size int
+}
+
+func NewKeyboard(size int) *Keyboard {
+	if size <= 0 {
+		return nil
+	}
+
+	words := (size + 16 - 1) / 16
+	return &Keyboard{keys: make([]uint16, words), size: size}
+}
+
+func (k *Keyboard) Press(key int) {
+	if key < 0 || key >= k.size {
+		return
+	}
+
+	word := key / 16
+	bit := uint16(key % 16)
+
+	k.keys[word] |= uint16(1) << bit
+}
+
+func (k *Keyboard) Release(key int) {
+	if key < 0 || key >= k.size {
+		return
+	}
+
+	word := key / 16
+	bit := uint16(key % 16)
+
+	k.keys[word] &^= uint16(1) << bit
+}
+
+func (k *Keyboard) IsPressed(key int) bool {
+	if key < 0 || key >= k.size {
+		return false
+	}
+
+	word := key / 16
+	bit := uint16(key % 16)
+
+	return (k.keys[word] & (uint16(1) << bit)) != 0
+}
+
+func (k *Keyboard) AnyPressed() (int, bool) {
+	for i, word := range k.keys {
+		if word == 0 {
+			continue
+		}
+
+		b := bits.TrailingZeros16(word)
+		key := i*16 + b
+		if key < k.size {
+			return key, true
+		}
+		return 0, false
+	}
+	return 0, false
+}
